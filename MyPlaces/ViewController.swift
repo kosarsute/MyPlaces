2f92b84a702ba9a6e9be86c50c2ed48d1808db08
Sergei Kosarev <89865368+kosarsute@users.noreply.github.com>
2022-06-08 22:42:52 -0700

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by SERGEI KOSAREV on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
